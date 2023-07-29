868934245ddfe29637216d7798025c6fc38896bd
Lee Zhongjun <80515759+leezhongjun@users.noreply.github.com>
2023-07-29 11:45:30 +0800

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -187,7 +187,7 @@ export default function Login() {
           {/* </div> */}
           <div className="mt-6">
             <input
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-600"
+              className="w-full text-sm font-medium px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-600"
               type="submit"
               value="Login"
             />
