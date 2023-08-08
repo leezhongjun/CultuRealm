cc5576f98b06f90376d81b2cd316f5cb4c7d40c5
Yi <chen.yichen.hello@icloud.com>
2023-08-08 12:50:57 +0800

    complete play story landing page(not finalised)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -58,7 +58,7 @@ function App() {
         </div>
       </div>
       <div>
-        <ParticlesBg type="square" bg={true} />
+      <ParticlesBg num={10} type="square" bg={true} />
       </div>
     </>
   );
--- frontend/src/pages/Story.tsx
+++ frontend/src/pages/Story.tsx
@@ -1,32 +1,82 @@
 import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
+import ParticlesBg from "particles-bg";
+import { Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  // let config = {
+  //   num: [1, 2],
+  //   rps: 0.1,
+  //   radius: [5, 40],
+  //   life: [1.5, 3],
+  //   v: [2, 3],
+  //   tha: [-40, 40],
+  //   // body: "./img/icon.png", // Whether to render pictures
+  //   // rotate: [0, 20],
+  //   alpha: [0.6, 0],
+  //   scale: [1, 0.1],
+  //   position: "center", // all or center or {x:1,y:1,width:100,height:100}
+  //   color: ["random", "#ff0000"],
+  //   cross: "dead", // cross or bround
+  //   random: 15,  // or null,
+  //   g: 5,    // gravity
+  //   // f: [2, -1], // force
+  //   onParticleUpdate: (ctx, particle) => {
+  //       ctx.beginPath();
+  //       ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+  //       ctx.fillStyle = particle.color;
+  //       ctx.fill();
+  //       ctx.closePath();
+  //   }
+  // };
+
   return (
     <>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          {/* Section header */}
+          <div className="text-center pb-12 md:pb-16">
+            <h1
+              className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              <span className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.5)] bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                CultuRealm
+              </span>
+            </h1>
+            <div className="max-w-3xl mx-auto">
+              <p
+                className="text-xl text-gray-800 mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                Begin your journey!
+              </p>
+              <div
+                className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+                data-aos="zoom-y-out"
+                data-aos-delay="300"
+              >
+                <div>
+                <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                      Enter Story
+                  </span>
+                </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ParticlesBg num={100} type="square" bg={true} />
+        {/* <ParticlesBg type="custom" config={config} bg={true} /> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR story
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
